3743d82bd5ac16cfa9ece97a7b8f4b217c57641c
shahmir-k <89569533+shahmir-k@users.noreply.github.com>
2025-05-21 08:43:04 -0400

    Added Files

---
+++ src/userInput.h
@@ -0,0 +1,17 @@
+
+#define MAX_DIR_LENGTH (1000 + 1)  // +1 for null terminator
+
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+// thanks to https://stackoverflow.com/questions/3219393/stdlib-and-colored-output-in-c
+
+
+int getUserInput(char *directory);
+int validateUserInput(char *directory);
+
